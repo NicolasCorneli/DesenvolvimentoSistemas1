d4cfb6bc98b158524a0c0c6014ea1c1900a8f748
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 20:14:08 -0300

    Create e2.ts

---
+++ typescript/exerciciosfixicacao/e2.ts
@@ -0,0 +1,13 @@
+enum DaysOfWeek{
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-Feira",
+  "Sexta-Feira",
+  "Sábado"
+}
+
+let dia: DaysOfWeek = DaysOfWeek["Quarta-Feira"];
+
+console.log(dia);
