34c1be54e73e55d36594b0f65984aa781cc78aa0
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:11:20 -0300

    Update e3.ts

--- typescript/exerciciosfixicacao/e3.ts
+++ typescript/exerciciosfixicacao/e3.ts
@@ -1,3 +1,5 @@
+// Uso de Arrays e Tuplas: Crie uma tupla chamada car que contenha o nome do carro (string) e o ano de fabricação (number). Depois, acesse e exiba esses valores no console.
+
 let car: [string,number] = ["Corolla",234324];
 
 for (let x in car){
