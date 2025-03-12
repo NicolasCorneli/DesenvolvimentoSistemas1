0513b32f726e34db4f59b98aff757737a4f1f8d3
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:52:02 -0300

    Create e9.ts

---
+++ typescript/exerciciosfixicacao/e9.ts
@@ -0,0 +1,7 @@
+// Tipos Literais: Crie um tipo literal chamado TrafficLight com os valores "red", "yellow" e "green". Em seguida, crie uma variável chamada currentLight e atribua um valor válido.
+
+type TrafficLight = "red" | "yellow" | "green";
+
+let currentLight: TrafficLight = "red";
+
+console.log(currentLight);
