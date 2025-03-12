f3b04690570e458ed2f16035d715867f91702bd4
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:20:36 -0300

    Create e5.ts

---
+++ typescript/exerciciosfixicacao/e5.ts
@@ -0,0 +1,5 @@
+// Arrow Function e Template Literals: Crie uma arrow function chamada greet que receba um nome e retorne uma saudação no formato Olá, [nome]!. Utilize template literals para formatar a mensagem.
+
+const greet = (nome:string): string => `Olá,${nome}`;
+
+console.log(greet("Emilia"));
