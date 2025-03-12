08b1adbeb819f09d45d9e3e99383e197e4c5d6e9
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:11:43 -0300

    Update e4.ts

--- typescript/exerciciosfixicacao/e4.ts
+++ typescript/exerciciosfixicacao/e4.ts
@@ -1,3 +1,5 @@
+// Função com Retorno: Crie uma função chamada multiply que recebe dois números como parâmetros e retorna o produto deles. Chame essa função e exiba o resultado.
+
 function multiply(x: number, y: number): number {
     return x * y;
 }
