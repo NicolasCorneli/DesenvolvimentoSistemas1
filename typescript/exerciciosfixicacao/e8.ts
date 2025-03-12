2f8f7e7e6274cab3d2980a434e9e7ed6937b51ee
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:47:17 -0300

    Create e8.ts

---
+++ typescript/exerciciosfixicacao/e8.ts
@@ -0,0 +1,7 @@
+// Desestruturação de Objetos: Crie um objeto person com propriedades name e age. Em seguida, use a desestruturação para extrair esses valores e exibi-los no console.
+const person = {name:"Nicolas", age:"20"};
+
+const {name, age} = person;
+
+console.log(name);
+console.log(age);
