e46d6532ad95430ca071c900e97cc4f523978b66
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:39:30 -0300

    Create e7.ts

---
+++ typescript/exerciciosfixicacao/e7.ts
@@ -0,0 +1,7 @@
+// Uso de Never: Crie uma função chamada errorMessage que sempre lança um erro com a mensagem "Erro crítico!".
+
+function errorMessage(): never {
+    throw new Error("Erro crítico!!");
+}
+
+errorMessage();
