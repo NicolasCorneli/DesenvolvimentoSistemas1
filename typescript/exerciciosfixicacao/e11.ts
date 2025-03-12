3619270587be6670b921220e7d8a039f92756b92
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 22:04:53 -0300

    Create e11.ts

---
+++ typescript/exerciciosfixicacao/e11.ts
@@ -0,0 +1,6 @@
+// Criando um Objeto Simples: Crie um objeto chamado carro que tenha as propriedades marca (string) e ano (number). Exiba essas propriedades no console.
+
+const carro = {marca: "Peugeot", ano: 2019};
+
+console.log(carro.marca);
+console.log(carro.ano);
