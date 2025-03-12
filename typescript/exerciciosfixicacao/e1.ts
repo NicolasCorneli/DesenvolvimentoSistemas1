cbf2f141aa97be57c701a53f8bafc002d884450e
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-03-11 21:10:14 -0300

    Update e1.ts

--- typescript/exerciciosfixicacao/e1.ts
+++ typescript/exerciciosfixicacao/e1.ts
@@ -1,3 +1,4 @@
+// Definição de Tipos Primitivos: Crie três variáveis com os seguintes tipos: string, number, e boolean. Atribua valores a elas e exiba-os no console.
 let v1: string = "Hello";
 let v2: number = 235478359;
 let v3: boolean = false;
